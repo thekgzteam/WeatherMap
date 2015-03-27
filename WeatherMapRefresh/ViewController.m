408a4d67f8a6e9a94a49266b81ed948085e6367e
benbueltmann <benbueltmann@gmail.com>
2015-03-27 14:33:41 -0500

    Initial Commit

---
+++ WeatherMapRefresh/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeatherMapRefresh
+//
+//  Created by Ben Bueltmann on 3/27/15.
+//  Copyright (c) 2015 Mobile Makers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
